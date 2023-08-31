fd356820efbb749589ee6a0fa5dea6bb06cbda92
kathirvelrajmohan <kathirvelrajmohan9498@gmail.com>
2023-08-31 19:30:09 +0530

    Single inheritance

---
+++ Assingnments/Day17 - OOPS-II/SingleInheritance.py
@@ -0,0 +1,20 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def printDetails(self):
+        print(self.name,self.reg)
+class Student(Person):
+    def __init__(self, name, age, standard, section):
+        super().__init__(name, age)
+        self.standard = standard
+        self.section = section 
+    def printDetails(self):
+        print("Name:",self.name)
+        print("Age:",self.age)
+        print("Standard:",self.standard)
+        print("Section:",self.section)
+a = Student("kathirvel",20,"CSE","C")
+b = Student("Thariq",25,"ECE","B")
+a.printDetails()
+b.printDetails()
